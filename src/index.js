db1fd535837d00a93865b34b4fa07eabeaf58ff7
Robert Luiz <robertlsc2016@gmail.com>
2025-07-08 19:17:26 -0300

    fix: correct privateKey assignment in ImageKit configuration

--- src/index.js
+++ src/index.js
@@ -19,7 +19,7 @@ const upload = multer(); // usa memória (buffer)
 
 const imagekit = new ImageKit({
   publicKey: process.env.IMAGEKIT_PUBLIC_KEY,
-  privateKey: process.env.IMAGEKIT_PUBLIC_KEY,
+  privateKey: process.env.IMAGEKIT_PRIVATE_KEY,
   urlEndpoint: process.env.IMAGEKIT_URL_ENDPOINT,
 });
 
